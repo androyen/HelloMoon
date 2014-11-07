628fd14ac58b58591b23fdde99bcf0515a8746dd
Androyen <androyen@aol.com>
2014-11-06 21:10:55 -0600

    Create Fragment in layout file

---
+++ app/src/main/java/com/androyen/hellomoon/HelloMoonFragment.java
@@ -0,0 +1,31 @@
+package com.androyen.hellomoon;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * Created by rnguyen on 11/6/14.
+ */
+public class HelloMoonFragment extends Fragment {
+
+    //Play and Stop buttons
+    private Button mPlayButton;
+    private Button mStopButton;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_hello_moon, parent, false);
+
+        //reference Play and Stop buttons
+        mPlayButton = (Button)v.findViewById(R.id.hellomoon_playButton);
+        mStopButton = (Button)v.findViewById(R.id.hellomoon_stopButton);
+
+        return v;
+    }
+}
